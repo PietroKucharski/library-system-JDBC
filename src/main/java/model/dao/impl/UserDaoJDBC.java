a078065546e511737163211c74f99221f9f692c1
pietr <pietrokucharski@hotmail.com>
2025-07-05 19:09:49 -0300

    feat: creating logic for the User insert method and creating method and logic to validate cpf

--- src/main/java/model/dao/impl/UserDaoJDBC.java
+++ src/main/java/model/dao/impl/UserDaoJDBC.java
@@ -1,9 +1,11 @@
 package model.dao.impl;
 
+import database.DatabaseConnection;
+import database.exceptions.DbException;
 import model.dao.UserDao;
 import model.entities.User;
 
-import java.sql.Connection;
+import java.sql.*;
 import java.util.List;
 
 public class UserDaoJDBC implements UserDao {
@@ -14,8 +16,32 @@ public class UserDaoJDBC implements UserDao {
     }
 
     @Override
-    public void insert(User author) {
+    public void insert(User user) {
+        PreparedStatement preparedStatement = null;
 
+        try {
+            if (!isValidCPF(user.getCpf())) {
+                throw new DbException("CPF inválido!");
+            }
+
+            preparedStatement = connection.prepareStatement("INSERT INTO users (name, email, cpf) VALUES(?, ?, ?);", Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getEmail());
+            preparedStatement.setString(3, user.getCpf());
+
+            int affectedRows = preparedStatement.executeUpdate();
+
+            if (affectedRows > 0) {
+                ResultSet resultSet = preparedStatement.getGeneratedKeys();
+                if (resultSet.next()) {
+                    int id = resultSet.getInt(1);
+                    user.setId(id);
+                }
+                DatabaseConnection.closeResultSet(resultSet);
+            }
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
     }
 
     @Override
@@ -36,5 +62,40 @@ public class UserDaoJDBC implements UserDao {
     @Override
     public List<User> findAll() {
         return List.of();
+    }
+
+    private boolean isValidCPF(String cpf) {
+        cpf = cpf.replaceAll("\\D", "");
+
+        if (cpf.length() != 11 || cpf.matches("(\\d)\\1{10}")) {
+            return false;
+        }
+
+        try {
+            int soma = 0;
+            for (int i = 0; i < 9; i++) {
+                soma += Character.getNumericValue(cpf.charAt(i)) * (10 - i);
+            }
+
+            int primeiroDigito = 11 - (soma % 11);
+            if (primeiroDigito >= 10) {
+                primeiroDigito = 0;
+            }
+
+            soma = 0;
+            for (int i = 0; i < 10; i++) {
+                soma += Character.getNumericValue(cpf.charAt(i)) * (11 - i);
+            }
+
+            int segundoDigito = 11 - (soma % 11);
+            if (segundoDigito >= 10) {
+                segundoDigito = 0;
+            }
+
+            return primeiroDigito == Character.getNumericValue(cpf.charAt(9)) &&
+                    segundoDigito == Character.getNumericValue(cpf.charAt(10));
+        } catch (Exception e) {
+            throw new DbException(e.getMessage());
+        }
     }
 }
