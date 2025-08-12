54ade62e27bcc276cfd8148910beca2b0ac8af6f
pietr <pietrokucharski@hotmail.com>
2025-08-12 12:26:30 -0300

    feat: implementing the logic of the insert method of the LoanDaoJDBC class

--- src/main/java/model/dao/impl/LoanDaoJDBC.java
+++ src/main/java/model/dao/impl/LoanDaoJDBC.java
@@ -1,11 +1,13 @@
 package model.dao.impl;
 
+import database.DatabaseConnection;
+import database.exceptions.DbException;
 import model.dao.LoanDao;
 import model.entities.Book;
 import model.entities.Loan;
 import model.entities.User;
 
-import java.sql.Connection;
+import java.sql.*;
 import java.util.List;
 
 public class LoanDaoJDBC implements LoanDao {
@@ -17,7 +19,34 @@ public class LoanDaoJDBC implements LoanDao {
 
     @Override
     public void insert(Loan loan) {
+        PreparedStatement preparedStatement = null;
 
+        try {
+            preparedStatement = connection.prepareStatement("INSERT INTO public.loans (book_id, user_id, date_loan, " +
+                    "expected_return_date) VALUES(?, ?, ?, ?);", Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setInt(1, loan.getBook().getId());
+            preparedStatement.setInt(2, loan.getUser().getId());
+            preparedStatement.setDate(3, java.sql.Date.valueOf(loan.getDateLoan()));
+            preparedStatement.setDate(4, java.sql.Date.valueOf(loan.getExpectedReturnDate()));
+
+            int affectedRows = preparedStatement.executeUpdate();
+
+            if (affectedRows > 0) {
+                ResultSet resultSet = preparedStatement.getGeneratedKeys();
+
+                if (resultSet.next()) {
+                    int id = resultSet.getInt(1);
+                    loan.setId(id);
+                }
+                DatabaseConnection.closeResultSet(resultSet);
+            } else {
+                throw new DbException("Error while inserting loan");
+            }
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DatabaseConnection.closeStatement(preparedStatement);
+        }
     }
 
     @Override
